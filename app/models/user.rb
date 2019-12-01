f53d988b45876619971d69711b1627b68129b682
Inon Bercu <inonbr@gmail.com>
2019-12-01 18:02:27 +0200

    added validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username,
+            uniqueness: true,
+            presence: true,
+            allow_blank: false,
+            length: { minimum: 3 }
 end
