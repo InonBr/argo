5a2391dc63a4a38425f0230969260f7c4304ad19
Inon Bercu <inonbr@gmail.com>
2019-12-01 17:25:19 +0200

    created maigrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_01_150423) do
+ActiveRecord::Schema.define(version: 2019_12_01_151355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,8 +54,18 @@ ActiveRecord::Schema.define(version: 2019_12_01_150423) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "words", force: :cascade do |t|
+    t.string "original"
+    t.string "translation"
+    t.bigint "language_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["language_id"], name: "index_words_on_language_id"
+  end
+
   add_foreign_key "quizzes", "languages"
   add_foreign_key "quizzes", "users"
   add_foreign_key "user_languages", "languages"
   add_foreign_key "user_languages", "users"
+  add_foreign_key "words", "languages"
 end
