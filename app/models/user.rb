9106239b2a605019bcc0ddf8da67eccfa5b421f5
Inon Bercu <inonbr@gmail.com>
2019-12-01 15:46:27 +0200

    added user modle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_many :user_words, dependent: :destroy
+  has_many :words, through: :user_words, dependent: :destroy
+  has_many :user_languages, dependent: :destroy
+  has_many :languages, through: :user_languages, dependent: :destroy
+  has_many :quizs, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
