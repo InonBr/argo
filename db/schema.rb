db41c6298039e7e9007e40ed4e7d43b968a6650b
Inon Bercu <inonbr@gmail.com>
2019-12-02 12:01:30 +0200

    finsished adding the back ground to the app. still working on the languages controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_01_153141) do
+ActiveRecord::Schema.define(version: 2019_12_02_092004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_12_01_153141) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "flag_url"
   end
 
   create_table "quizzes", force: :cascade do |t|
