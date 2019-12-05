de0de81d986776b9fdc69f8ec771f5c1bc3cda5e
Inon Bercu <inonbr@gmail.com>
2019-12-05 12:59:39 +0200

    fixed nevnar redirecting

--- app/controllers/user_languages_controller.rb
+++ app/controllers/user_languages_controller.rb
@@ -1,7 +1,9 @@
 class UserLanguagesController < ApplicationController
   def show
+    unless params['id'] == '#'
     @user_language = UserLanguage.find(params['id'])
     authorize @user_language
+    end
     @random_word_in_user_language = Word.random_unseen(current_user)
     @can_take_quiz = UserWord.where(user: current_user, quizzed: false, removed: false, knew: true).count >= 5
   end
@@ -15,10 +17,10 @@ class UserLanguagesController < ApplicationController
     if current_user.languages.include?(@language)
       # make the user languge that he has active
       # and all others active = false
-      chosen_user_language = current_user.user_languages.find_by(language: @language)
-      chosen_user_language.active = true
-      chosen_user_language.save
-      authorize chosen_user_language
+       @user_language = current_user.user_languages.find_by(language: @language)
+       @user_language.active = true
+       @user_language.save
+      authorize  @user_language
     else
       # create a new user language and make it active
       @user_language = UserLanguage.create(user: current_user, language: @language, active: true)
@@ -33,6 +35,6 @@ class UserLanguagesController < ApplicationController
         l.save
       end
     end
-    redirect_to user_language_path(chosen_user_language)
+    redirect_to user_language_path( @user_language)
   end
 end
