fdeac0132492ed9f8ceac01ff7b6609641c086f7
jeremybeyon <themsbun@gmail.com>
2019-12-06 21:20:59 +0200

    userlist

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,12 +1,18 @@
-class DashboardsController < ApplicationController
+ class DashboardsController < ApplicationController
   def profile
     @user = current_user
     @scores = {}
+    @totals = {}
     @user.quizzes.each do |quiz|
+      #each score start from zero and makes sure it doesn't start over
+      #the times are in seconds
+      #it has iterations
+      #takeing the sum of all the points
+      #makes sure it gets the proper score at the end
       @scores[quiz.language.name] = ((@scores[quiz.language.name] || 0) + quiz.score)
-      @scores["total_study_time"] = @scores["total_study_time"] || 0 + (quiz.updated_at - quiz.created_at)
+      @totals["total_study_time"] = @totals["total_study_time"] || 0 + (quiz.updated_at - quiz.created_at)
     end
-    @scores["total_points"] = @user.quizzes.map {|quiz| quiz.score}.sum
-
+    @totals["total_points"] = @user.quizzes.map {|quiz| quiz.score}.sum
+    raise
   end
 end
