224e984d0b855fe300168644147209642050db95
jeremybeyon <themsbun@gmail.com>
2019-12-06 18:52:50 +0200

    dashboardscore

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,12 @@
 class DashboardsController < ApplicationController
   def profile
     @user = current_user
+    @scores = {}
+    @user.quizzes.each do |quiz|
+      @scores[quiz.language.name] = ((@scores[quiz.language.name] || 0) + quiz.score)
+      @scores["total_study_time"] = @scores["total_study_time"] || 0 + (quiz.updated_at - quiz.created_at)
+    end
+    @scores["total_points"] = @user.quizzes.map {|quiz| quiz.score}.sum
+
   end
 end
