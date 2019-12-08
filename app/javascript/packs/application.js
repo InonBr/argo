a1140cf3f2c56c64186ef1d3f9312071541a703f
Ophira Blumner <ophirablumner@gmail.com>
2019-12-08 14:42:36 +0200

    no thanks words don't show up in user word list anymore

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,14 @@ if(document.querySelector('.quiz-answers')) {
 
 import { setupUserWordsForm } from '../plugins/create-user-word'
 import { setupCardFlipper } from '../plugins/flip-word-card'
+import { setupNoThanks } from '../plugins/remove_user_word'
 
 if (document.querySelector('form.new_user_word')) {
   setupUserWordsForm()
+  setupNoThanks()
 }
 
 if (document.querySelector('.flipper')) {
   setupCardFlipper()
 }
+
